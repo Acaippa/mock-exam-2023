7092899a388fedc24969814df5ddccf18787fe6b
Nils <olivernils8@gmail.com>
2023-02-15 10:06:43 +0100

    [ADD] Added ticket models

--- server/ticket/admin.py
+++ server/ticket/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ticket, Teknikker, Avdeling, Status
 
 # Register your models here.
+admin.site.register(Ticket)
+admin.site.register(Teknikker)
+admin.site.register(Avdeling)
+admin.site.register(Status)
--- server/ticket/models.py
+++ server/ticket/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from colorfield.fields import ColorField
 
-# Create your models here.
+class Avdeling(models.Model):
+    Navn = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Avdelinger"
+
+def get_default_avdeling():
+    return Avdeling.objects.get(navn="Tildelning")
+
+class Status(models.Model):
+    Navn = models.CharField(max_length=60)
+    Farge = ColorField(default="#FFFFFF")
+
+    class Meta:
+        verbose_name_plural = "Statuser"
+
+
+def get_default_status():
+    return Status.objects.get(navn="Åpen")
+
+class Teknikker(models.Model):
+    Navn = models.CharField(max_length=100)
+    Email = models.EmailField(max_length=100)
+    Avdeling = models.ForeignKey(Avdeling, default=get_default_avdeling, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name_plural = "Teknikkere"
+
+class Ticket(models.Model):
+    Emne = models.CharField(max_length=100)
+    Sender_epost = models.EmailField(max_length=100)
+    Sender_navn = models.CharField(max_length=100)
+    Melding = models.TextField(max_length=2000)
+    # Bilde = models.ImageField
+    Tildelt = models.ForeignKey(Teknikker, on_delete=models.DO_NOTHING, null=True, blank=True)
+    Tildelt_avdeling = models.ForeignKey(Avdeling, on_delete=models.DO_NOTHING, default=get_default_avdeling)
+    Dato_lagd = models.DateField(auto_now_add=True)
+    Status = models.ForeignKey(Status, default=get_default_status, on_delete=models.DO_NOTHING)
